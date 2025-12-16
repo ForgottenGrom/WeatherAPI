35f4ce01003833f582abcbafb730aece41b616d7
Bohdan Medvid <bohdanmedvid7@gmail.com>
2025-12-16 15:06:11 +0200

    add map and moved func

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 type WeatherData struct {
@@ -19,38 +20,62 @@ type OpenMeteoResponse struct {
 		WeatherCode float64 `json:"weathercode"`
 	} `json:"current_weather"`
 }
+type Geo struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
 
 func main() {
-	weather := WeatherData{"London", 12.5, "Fog"}
-
-	jsonData, err := json.Marshal(weather)
-	if err != nil {
-		log.Fatal(err)
+	http.HandleFunc("/", getWeather)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	} else {
+		port = ":" + port
 	}
-	fmt.Println(string(jsonData))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		resp, _ := http.Get("https://api.open-meteo.com/v1/forecast?latitude=50.45&longitude=30.52&current_weather=true")
-		var response OpenMeteoResponse
-		defer resp.Body.Close()
-		body, _ := io.ReadAll(resp.Body)
-		json.Unmarshal(body, &response)
-		var desc string
-		switch response.CurrentWeather.WeatherCode {
-		case 0:
-			desc = "Clear sky"
-		default:
-			desc = "Cloudy/Rainy"
-		}
-		weatherResult := WeatherData{
-			City:        "Kyiv",
-			Temperature: response.CurrentWeather.Temperature,
-			Description: desc,
-		}
-		d, _ := json.Marshal(weatherResult)
-		w.Write(d)
-	})
+}
+func getWeather(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	m := map[string]Geo{
+		"Kyiv":    {Lat: 50.45, Lon: 30.52},
+		"London":  {Lat: 51.51, Lon: -0.13},
+		"NewYork": {Lat: 40.71, Lon: -74.01},
+	}
+	city := r.URL.Query().Get("city")
+	geo, ok := m[city]
+	if !ok {
+		http.Error(w, "City not found", http.StatusNotFound)
+		return
+	}
+	url := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current_weather=true", geo.Lat, geo.Lon)
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(w, "Failed to get weather data", http.StatusInternalServerError)
+		return
+	}
+	var response OpenMeteoResponse
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read weather data", http.StatusInternalServerError)
+		return
+	}
+	json.Unmarshal(body, &response)
+	var desc string
+	switch response.CurrentWeather.WeatherCode {
+	case 0:
+		desc = "Clear sky"
+	default:
+		desc = "Cloudy/Rainy"
+	}
+	weatherResult := WeatherData{
+		City:        city,
+		Temperature: response.CurrentWeather.Temperature,
+		Description: desc,
+	}
 
-	http.ListenAndServe(":8080", nil)
+	d, _ := json.Marshal(weatherResult)
+	w.Write(d)
 
 }
